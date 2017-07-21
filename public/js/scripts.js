40f595a24bdab587cd43cb1f96c98772b49874e4
triciahoover <tricia.l.hoover@gmail.com>
2017-07-21 15:00:17 -0400

    Alert not working

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,3 +1,12 @@
-// function alert() {     
-// 	alert("Hello! This is the math problem you are trying to do: #{num1} #{action} #{num2}");
-// }
+function mathAlert(num1, action, num2) {
+
+var numberOne = document.getElementById(num1).value
+var numberTwo = document.getElementById(num2).value
+var operator = document.getElementById(action).value.to_string
+// var numberOne = <%= num1 %>
+// var numberTwo = <%= num2 %>
+// var operator = <%= action %>.to_string
+
+alert('Hello! This is the math problem you are trying to do: ' + numberOne + ' ' + operator + ' ' + numberTwo );
+}
+
