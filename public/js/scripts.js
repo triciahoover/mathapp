d581136ad947a8106908b193327abe95e1786da0
triciahoover <tricia.l.hoover@gmail.com>
2017-07-21 15:46:17 -0400

    still not working

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,11 +1,10 @@
-function mathAlert(num1, action, num2) {
+function mathAlert() {
 
-var numberOne = document.getElementById(num1).value
-var numberTwo = document.getElementById(num2).value
-var operator = document.getElementById(action).value.to_string
-// var numberOne = <%= num1 %>
-// var numberTwo = <%= num2 %>
-// var operator = <%= action %>.to_string
+
+var numberOne = document.getElementById('num1');
+var numberTwo = document.getElementById('num2');
+var operator = document.getElementById('action');
+
 
 alert('Hello! This is the math problem you are trying to do: ' + numberOne + ' ' + operator + ' ' + numberTwo );
 }
